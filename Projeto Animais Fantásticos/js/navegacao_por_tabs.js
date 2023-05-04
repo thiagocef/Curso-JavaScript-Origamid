3fed3eae9c8a596e1bcece883b50731626d74beb
thiagocef <thicef@gmail.com>
2023-05-03 21:31:34 -0300

    nagevacao por tab

--- Projeto Animais Fantásticos/js/navegacao_por_tabs.js
+++ Projeto Animais Fantásticos/js/navegacao_por_tabs.js
@@ -1,18 +1,29 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+//dando dinamismo às imagens do site
+//OBS: foi criada uma nova classe chamada "js" para fazer a referência aos elementos sem alterar as classes anteriores
 
+//selecionando cada li do pai js-tabmenu
+//e cada section do pai js-tabcontent
+const tabMenu = document.querySelectorAll('.js-tabmenu li'),
+      tabContent = document.querySelectorAll('.js-tabcontent section')
+
+//a página sempre vai iniciar com esse elemento a mostra
+tabContent[0].classList.add('ativo')
+
+//função que insere a classe 'ativo'
 function activeTab(index) {
-
-    tabContent.forEach((section => {
+    //remove se houver a classe
+    tabContent.forEach(section => {
         section.classList.remove('ativo')
-    }))
-
+    })
+    //adiciona se não houver
     tabContent[index].classList.add('ativo')
 }
 
-
-tabMenu.forEach((itemMenu, index => {
-    itemMenu.addEventListenet('click', () => {
+//os index de tabMenu e tabContent foram organizados na mesma sequência
+tabMenu.forEach((itemMenu, index) => {
+    //adiciona um evento para cada item, retornando o index de cada um
+    //chama a função callback, que ativa a função activeTab, baseado no index
+    itemMenu.addEventListener('click', () => {
         activeTab(index)
-    })
-}))
+    })    
+});
